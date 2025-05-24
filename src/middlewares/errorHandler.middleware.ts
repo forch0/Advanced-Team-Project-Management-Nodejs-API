c7f528ad76ee061d9dccab4353694de1eba379c8
forch0 <forchh.dev@gmail.com>
2025-05-24 15:52:21 +0800

    errorhandler

---
+++ src/middlewares/errorHandler.middleware.ts
@@ -0,0 +1,62 @@
+// Copyright 2025 fortu
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     https://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { ErrorRequestHandler, Response } from "express";
+import { HTTPSTATUS } from "../config/http.config";
+import { AppError } from "../utils/appError";
+import { z, ZodError } from "zod";
+import { ErrorCodeEnum } from "../enums/error-code.enum";
+
+const formatZodError = (res: Response, error: z.ZodError) => {
+  const errors = error?.issues?.map((err) => ({
+    field: err.path.join("."),
+    message: err.message,
+  }));
+  return res.status(HTTPSTATUS.BAD_REQUEST).json({
+    message: "Validation failed",
+    errors: errors,
+    errorCode: ErrorCodeEnum.VALIDATION_ERROR,
+  });
+};
+
+export const errorHandler: ErrorRequestHandler = (
+  error,
+  req,
+  res,
+  next
+): any => {
+  console.error(`Error Occured on PATH: ${req.path} `, error);
+
+  if (error instanceof SyntaxError) {
+    return res.status(HTTPSTATUS.BAD_REQUEST).json({
+      message: "Invalid JSON format. Please check your request body.",
+    });
+  }
+
+  if (error instanceof ZodError) {
+    return formatZodError(res, error);
+  }
+
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      message: error.message,
+      errorCode: error.errorCode,
+    });
+  }
+
+  return res.status(HTTPSTATUS.INTERNAL_SERVER_ERROR).json({
+    message: "Internal Server Error",
+    error: error?.message || "Unknow error occurred",
+  });
+};
